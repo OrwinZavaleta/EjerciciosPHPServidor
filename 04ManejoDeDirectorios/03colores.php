f20bcc44946c07fd08a183e58c421ee9ac9d1c47
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-09-30 13:43:32 +0200

    Ejercicio 3

--- 04ManejoDeDirectorios/03colores.php
+++ 04ManejoDeDirectorios/03colores.php
@@ -2,7 +2,7 @@
 $PATH = "colores.txt";
 
 // ===== Escribiendo el archivo =====
-$file = fopen($PATH, "w");
+$file = fopen($PATH, "w+");
 
 if ($file) {
     if (flock($file, LOCK_EX)) {
@@ -16,24 +16,34 @@ if ($file) {
         echo "No se puede bloquear el archivo.";
     }
 
+
+    // ===== Leyendo el archivo =====
+    fseek($file, 0, SEEK_SET);
+    // $contenido = file_get_contents($PATH);
+    $contenido = fread($file, filesize($PATH));
+    if ($contenido) {
+        echo $contenido;
+    } else {
+        echo "No se pudo leer el archivo.";
+    }
+
+    // ===== Sobreescribiendo el archivo =====
+    // file_put_contents($PATH, "magenta\n");
+    // $contenido = file_get_contents($PATH);
+
+    ftruncate($file, 0); //borrarlo todo
+    fwrite($file, "magenta"); //escribir
+
+    fseek($file, 0, SEEK_SET); //cursor al inicio
+    $contenido = fread($file, filesize($PATH)); //leer
+
+    if ($contenido) {
+        echo $contenido;
+    } else {
+        echo "No se pudo leer el archivo.";
+    }
+
     fclose($file);
 } else {
     echo "No se puede abrir el archivo.";
-}
-
-// ===== Leyendo el archivo =====
-$contenido = file_get_contents($PATH);
-if ($contenido) {
-    echo $contenido;
-} else {
-    echo "No se pudo leer el archivo.";
-}
-
-// ===== Sobreescribiendo el archivo =====
-file_put_contents($PATH, "magenta\n");
-$contenido = file_get_contents($PATH);
-if ($contenido) {
-    echo $contenido;
-} else {
-    echo "No se pudo leer el archivo.";
 }
