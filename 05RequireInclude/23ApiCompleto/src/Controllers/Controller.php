e8cf9ff03475d09596c3c2226084649d604ea26f
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-11-17 14:29:26 +0100

    ejercicio de api con base de datos completo

---
+++ 05RequireInclude/23ApiCompleto/src/Controllers/Controller.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Database;
+use Dotenv\Dotenv;
+
+class Controller
+{
+
+    //Instanciar el modelo
+    private $myModel;
+
+    public function __construct()
+    {
+        error_log("hola");
+        $dotenv = Dotenv::createImmutable(__DIR__ . "/../..");
+        $dotenv->load();
+        $this->myModel = new Database($_ENV["DB_HOST"], $_ENV["DB_PORT"], $_ENV["DB_DATABASE"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
+        error_log("conectado");
+    }
+
+    public function getAll()
+    {
+        echo json_encode($this->myModel->loadDeps());
+    }
+    public function getId($id)
+    {
+        echo json_encode($this->myModel->loadDep($id));
+    }
+    public function create()
+    {
+        // $data = file_get_contents("php://input");
+        $request = json_decode(file_get_contents("php://input"), true);
+        $depart_no = $request["depart_no"];
+        $dnombre = $request["dnombre"];
+        $loc = $request["loc"];
+        echo json_encode($this->myModel->createDep($depart_no, $dnombre, $loc));
+    }
+    public function update($id) // TODO: falta terminarlo
+    {
+        $request = json_decode(file_get_contents("php://input"), true);
+        $depart_no = $request["depart_no"];
+        $dnombre = $request["dnombre"];
+        $loc = $request["loc"];
+        echo json_encode($this->myModel->updateDep($depart_no, $dnombre, $loc));
+    }
+    public function delete($id)
+    {
+        echo json_encode($this->myModel->deleteDep($id));
+    }
+}
