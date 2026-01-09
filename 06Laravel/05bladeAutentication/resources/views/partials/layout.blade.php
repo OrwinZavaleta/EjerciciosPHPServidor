674685935c38ab2da94d10528d46d9afec6d3de2
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-01-09 09:44:27 +0100

    creacion de las tablas de prieto eats

--- 06Laravel/05bladeAutentication/resources/views/partials/layout.blade.php
+++ 06Laravel/05bladeAutentication/resources/views/partials/layout.blade.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap 5.3.8 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link rel="shortcut icon" href="/img/gregorioprieto.png" type="image/x-icon">
     <title>@yield('title', 'Prieto Eats')</title>
 </head>
 
