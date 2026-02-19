b59382208873dbc13cc39e2817705cabfd6947ed
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-02-19 21:03:58 +0100

    Mejoras en la api

--- 07Nodejs/02Servidor/api.js
+++ 07Nodejs/02Servidor/api.js
@@ -14,20 +14,30 @@ const server = http.createServer(async (req, res) => {
 
         stream.pipe(res)
     } else if (method == "POST" && url == "/api/users") {
-        const usuarios = JSON.parse(await fsPro.readFile(PATH, { encoding: "utf8" }))
         let data = ""
         req.on("data", chunk => {
             data += chunk
         })
 
         req.on("end", async () => {
-            const datos = JSON.parse(data)
-            usuarios.push(datos);
-            await fsPro.writeFile(PATH, JSON.stringify(usuarios))
-            console.log(usuarios);
+            try {
+                const datos = JSON.parse(data)
+                const usuarios = JSON.parse(await fsPro.readFile(PATH, { encoding: "utf8" }) || "[]")
 
-            res.writeHead(200, { "content-type": "application/json" })
-            res.end(JSON.stringify({ message: "Usuario agregado correctamente" }))
+                usuarios.push(datos);
+                await fsPro.writeFile(PATH, JSON.stringify(usuarios))
+
+                console.log(usuarios);
+
+                res.writeHead(200, { "content-type": "application/json" })
+                res.end(JSON.stringify({ message: "Usuario agregado correctamente" }))
+            } catch (error) {
+
+                console.error(error)
+
+                res.writeHead(400, { "content-type": "application/json" })
+                res.end(JSON.stringify({ message: "Error con el usuario" }))
+            }
         })
     } else {
         res.writeHead(404, { "content-type": "application/json" })
@@ -38,12 +48,3 @@ const server = http.createServer(async (req, res) => {
 server.listen(3000, () => {
     console.log("servidor iniciadoen el puerto 3000");
 })
-
-// async function leerUsuarios() {
-//     const users = JSON.parse(await fs.readFile(, { encoding: "utf8" }))
-//     return users;
-// }
-
-// async function guardarUsuario() {
-
-// }
